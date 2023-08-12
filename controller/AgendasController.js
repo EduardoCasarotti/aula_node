5f822c6de50aec620c0306de860ee16b14c74e69
EduardoCasarotti <eduardocasarottiii@gmail.com>
2023-08-11 22:42:34 -0300

    Incluindo insert e atualizando as rotas

--- controller/AgendasController.js
+++ controller/AgendasController.js
@@ -1,12 +1,45 @@
 const { json } = require("express");
 const db = require("../database/conection");
 
-module.exports ={
-    async listarAgendas(request, response) {
-        try{
-            return response.status(200).json({confirma: "Agendas"});
-        } catch (error) {
-            return response.status(500).json({confirma: "Erro" , message: error});
-        }
-    },
+module.exports = {
+  async listarAgendas(request, response) {
+    try {
+      const sql =
+       'SELECT agd_id, pess_id, serv_id, agd_dia, agd_horario, agd_pagamento, agd_valor_pago from agendas'  ;
+      const agendas = await db.query(sql);
+
+      return response.status(200).json(agendas[0]);
+    } catch (error) {
+      return response.status(500).json({ confirma: "Erro", message: error });
+    }
+  },
+  async CadastroAgendas(request, response) {
+    try {
+
+      return response.status(200).json({ confirma: "Cadastrar Agendas" });
+    } catch (error) {
+      return response.status(500).json({ confirma: "Erro", message: error });
+    }
+  },
+  async EditarAgendas(request, response) {
+    try {
+      return response.status(200).json({ confirma: "Editar Agendas" });
+    } catch (error) {
+      return response.status(500).json({ confirma: "Erro", message: error });
+    }
+  },
+  async EditarAgendas(request, response) {
+    try {
+      return response.status(200).json({ confirma: "Editar Agendas" });
+    } catch (error) {
+      return response.status(500).json({ confirma: "Erro", message: error });
+    }
+  },
+  async ExcluirAgendas(request, response) {
+    try {
+      return response.status(200).json({ confirma: "Excluir Agendas" });
+    } catch (error) {
+      return response.status(500).json({ confirma: "Erro", message: error });
+    }
+  },
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,11 @@
-const db = require("../database/conetion");
+const db = require("../database/conection");
 const express = require("express");
 const router = express.Router();
 
 //importação dos controlers utilizados nas rotas
 
 const AgendasController = require("../controller/AgendasController");
-const EstabelecimentosController = require("../controller/EstabelecimentosControlle");
+const EstabelecimentosController = require("../controller/EstabelecimentosController");
 const PessoasController = require("../controller/PessoasController");
 const ServicosController = require("../controller/ServicosController");
 const TelefonesController = require("../controller/TelefonesController");
@@ -13,34 +13,36 @@ const TelefonesController = require("../controller/TelefonesController");
 //Definindo as rotas
 
 router.get("/Agendas", AgendasController.listarAgendas);
-//cadastrar
-//editar
-//excluir
+router.post("/Agendas", AgendasController.CadastroAgendas);
+router.patch("/Agendas", AgendasController.EditarAgendas);
+router.delete("/Agendas", AgendasController.ExcluirAgendas);
+
 module.exports = router;
 
-router.get(
-  "/Estabelecimentos",
-  EstabelecimentosController.listarEstabelecimentos
-);
-//cadastrar
-//editar
-//excluir
+router.get("/Estabelecimentos",EstabelecimentosController.listarEstabelecimentos);
+router.post("/Estabelecimentos",EstabelecimentosController.CadastroEstabelecimentos);
+router.patch("/Estabelecimentos",EstabelecimentosController.EditarEstabelecimentos);
+router.delete("/Estabelecimentos",EstabelecimentosController.ExcluirEstabelecimentos);
+
 module.exports = router;
 
 router.get("/Pessoas", PessoasController.listarPessoas);
-//cadastrar
-//editar
-//excluir
+router.post("/Pessoas", PessoasController.CadastroPessoas);
+router.patch("/Pessoas", PessoasController.EditarPessoas);
+router.delete("/Pessoas", PessoasController.ExcluirPessoas);
+
 module.exports = router;
 
-router.get("/Serviços", ServicosController.listarServicos);
-//cadastrar
-//editar
-//excluir
+router.get("/Servicos", ServicosController.listarServicos);
+router.post("/Servicos", ServicosController.CadastroServicos);
+router.patch("/Servicos", ServicosController.EditarServicos);
+router.delete("/Servicos", ServicosController.ExcluirServicos);
+
 module.exports = router;
 
 router.get("/Telefones", TelefonesController.listarTelefones);
-//cadastrar
-//editar
-//excluir
+router.post("/Telefones", TelefonesController.CadastroTelefones);
+router.patch("/Telefones", TelefonesController.EditarTelefones);
+router.delete("/Telefones", TelefonesController.ExcluirPessoas);
+
 module.exports = router;
